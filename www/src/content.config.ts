1dfb90eed6449c78148d479055312e88e425c8fc
Shravan Goswami <shravanngoswamii@gmail.com>
2026-07-27 18:55:58 +0530

    make www consume the package instead of carrying its own copy of the site

--- www/src/content.config.ts
+++ www/src/content.config.ts
@@ -1,25 +1,7 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { blogLoader, blogSchema, docsLoader, docsSchema } from "almanac/content";
 
-const docs = defineCollection({
-	loader: glob({ base: "./docs", pattern: "**/*.md" }),
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-	}),
-});
-
-const blog = defineCollection({
-	loader: glob({ base: "./blog", pattern: "**/*.md" }),
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			tags: z.array(z.string()).optional(),
-			heroImage: z.union([image(), z.string()]).optional(),
-		}),
-});
-
-export const collections = { docs, blog };
+export const collections = {
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	blog: defineCollection({ loader: blogLoader(), schema: blogSchema() }),
+};
